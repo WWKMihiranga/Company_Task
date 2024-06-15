65c808f8442f30a11f007043b4e66dcb2c8bab30
Kavindu Mihiranga <kavirangot@gmail.com>
2024-06-15 14:02:16 +0530

    Update the delete icon

--- lib/widgets/item_list.dart
+++ lib/widgets/item_list.dart
@@ -6,19 +6,19 @@ import '../utils/colors.dart';
 
 import '../providers/item_provider.dart';
 
-class ItemList extends StatelessWidget { // Define a StatefulWidget for displaying a list of items.
+class ItemList extends StatelessWidget {
+  // Define a StatefulWidget for displaying a list of items.
   final List<Item> items;
 
-  ItemList({ // Constructor
-    required this.items
-  });
+  ItemList(
+      { // Constructor
+      required this.items});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: items.length, // Total number of items in the list.
       itemBuilder: (ctx, index) {
-
         return Card(
           color: PastelColors.pastel[200], // Background color of the card.
           child: ListTile(
@@ -29,27 +29,32 @@ class ItemList extends StatelessWidget { // Define a StatefulWidget for displayi
               ),
             ),
             onTap: () {
-              Navigator.of(context).push( // Navigate to DetailScreen when item is tapped.
+              Navigator.of(context).push(
+                // Navigate to DetailScreen when item is tapped.
                 MaterialPageRoute(
                   builder: (context) => DetailScreen(item: items[index]),
                 ),
               );
             },
-
-            trailing: IconButton( // Trailing icon button for deleting the item.
-              icon: Icon(Icons.delete), // Delete icon.
+            trailing: IconButton(
+              // Trailing icon button for deleting the item.
+              icon: Icon(
+                Icons.delete,
+                color: PastelColors.pastel[800],
+                size: 32,
+              ), // Delete icon.
               onPressed: () {
                 // Delete the item from the list
-                Provider.of<ItemProvider>(context, listen: false).deleteItem(items[index].id);
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar( // Display a confirmation message.
+                Provider.of<ItemProvider>(context, listen: false)
+                    .deleteItem(items[index].id);
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  // Display a confirmation message.
                   content: Text('Deleted successfully'),
                 ));
               },
             ),
-
           ),
         );
-
       },
     );
   }
